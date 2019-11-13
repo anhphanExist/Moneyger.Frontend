ad5500ab8b16e2779c06f05322bbd3dca6bbdf58
Anh Phan <anhphan.csf@gmail.com>
2019-11-14 00:51:24 +0700

    init all mockups

--- money-lover-clone-master/src/router.js
+++ money-lover-clone-master/src/router.js
@@ -7,6 +7,10 @@ import Authenticated from "./views/Authenticated.vue";
 import Transaction from "./components/transaction/Transaction.vue";
 import Wallet from "./components/wallet/Wallet.vue";
 import AddTransaction from "./components/transaction/AddTransaction.vue";
+import TransferTransaction from "./components/transaction/TransferTransaction.vue";
+import EditProfile from "./components/user/EditProfile.vue";
+import AddWallet from "./components/wallet/AddWallet.vue";
+import EditWallet from "./components/wallet/AddWallet.vue";
 
 Vue.use(VueRouter);
 
@@ -47,29 +51,49 @@ const routes = [
       }
     },
     children: [
-        {
-            path: "transaction",
-            name: "transaction",
-            component: Transaction
-        },
-        {
-            path: "add-transaction",
-            name: "addTransaction",
-            component: AddTransaction
-        },
-        {
-            path: "wallet",
-            name: "wallet",
-            component: Wallet
-        },
-        {
-            path: "",
-            redirect: "transaction"
-        },
-        {
-          path: "*",
-          redirect: "transaction"
-        }
+      {
+        path: "transaction",
+        name: "transaction",
+        component: Transaction
+      },
+      {
+        path: "add-transaction",
+        name: "addTransaction",
+        component: AddTransaction
+      },
+      {
+        path: "wallet",
+        name: "wallet",
+        component: Wallet
+      },
+      {
+        path: "add-wallet",
+        name: "addWallet",
+        component: AddWallet
+      },
+      {
+        path: "edit-wallet",
+        name: "editWallet",
+        component: EditWallet
+      },
+      {
+        path: "transfer-transaction",
+        name: "transferTransaction",
+        component: TransferTransaction
+      },
+      {
+        path: "edit-profile",
+        name: "editProfile",
+        component: EditProfile
+      },
+      {
+        path: "",
+        redirect: "transaction"
+      },
+      {
+        path: "*",
+        redirect: "transaction"
+      }
     ]
   },
   {
@@ -83,6 +107,6 @@ const routes = [
 ];
 
 export default new VueRouter({
-  mode: 'history', 
+  mode: "history",
   routes
 });
